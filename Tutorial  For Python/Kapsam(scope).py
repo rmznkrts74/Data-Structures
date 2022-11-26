828c4550e21e28133c8b66e1f09b6098669fcb34
rmznkrts74 <92362712+rmznkrts74@users.noreply.github.com>
2022-11-27 00:39:41 +0300

    dada

--- Tutorial  For Python/Kapsam(scope).py
+++
@@ -1,6 +0,0 @@
-#scope:local,enclosing,global,built in
-no =20
-def carpma(rakam):
-    no2=10
-    return no2*rakam
-print(carpma(5))
--- Tutorial  For Python/tuple.py
+++
@@ -1,5 +0,0 @@
-my_Tuple=(1,2,"a",4.5)
-print((my_Tuple))
-print(my_Tuple[0])
-#my_Tuple[0]=3#immutable so this error
-print((my_Tuple[0]))
